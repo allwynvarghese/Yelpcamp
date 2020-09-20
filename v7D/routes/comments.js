278e2f5780f959de9d4424892272bc3d464e4e20
allwyn varghese <allwynvarghese@gmail.com>
2020-09-20 14:23:16 +0000

    v7 deployment

---
+++ v7D/routes/comments.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const router = express.Router({mergeParams: true});
+const Campground = require("../models/Campground");
+const Comments = require("../models/Comments");
+const middleware = require("../middleware");
+
+//NEW comment
+router.get("/camps/:id/comment/new", isLoggedIn, (req,res)=>{
+	Campground.findById(req.params.id, (err, campground)=>{
+		if(err || !campground){
+			console.log(err);
+			req.flash("error", "Oops! We are unable to find this comment!!");
+			res.redirect("/camps");
+		}else{
+			res.render("comment/new", {camp: campground})
+		}
+	});
+});
+
+//CREATE new comment
+router.post("/camps/:id/comment", isLoggedIn, (req, res)=>{
+	//1. find campground by id
+	Campground.findById(req.params.id, (err, campground)=>{
+		if(err || !campground){
+			console.log("err");
+			req.flash("error", "Oops! We are unable to find this comment!!");
+			res.redirect("back");
+		}else{
+			//2. Create the comment
+			Comments.create(req.body.comment, (err, comment)=>{
+				if(err){
+					console.log(err);
+				}else{
+					//add user to comments
+					comment.author.id = req.user._id;
+					comment.author.username = req.user.username;
+					//save the comments
+					comment.save();
+					//push comment into campground
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/camps/" + campground._id)
+				}
+			});
+		}
+	});
+});
+
+//Edit Comments
+router.get("/camps/:id/comment/:comment_id/edit", middleware.checkCommentAuth, (req, res)=>{
+	Campground.findById(req.params.id, (err, foundCamps)=>{
+		if(err || !foundCamps){
+			console.log(err);
+			req.flash("error", "Oops! We are unable to find this comment!!");
+			res.redirect("back");
+		}else{
+			Comments.findById(req.params.comment_id, (err, foundComment)=>{
+				res.render("comment/edit", {camp: foundCamps, comment: foundComment});
+			});
+		}
+	});
+	
+});
+
+//Add edited comments
+router.put("/camps/:id/comment/:comment_id", (req, res)=>{
+	Comments.findByIdAndUpdate(req.params.comment_id, req.body.comment, middleware.checkCommentAuth, (err, updatedComment)=>{
+		if(err || !updatedComment){
+			console.log(err);
+			if(!updatedComment){
+				req.flash("error", "Oops! We are unable to find this comment!!");
+			}
+			res.redirect("back");
+		}else{
+			res.redirect("/camps/" + req.params.id);
+		}
+	});
+});
+
+//Destroy comments
+router.delete("/camps/:id/comment/:comment_id", middleware.checkCommentAuth, (req,res)=>{
+	Comments.findByIdAndRemove(req.params.comment_id, (err)=>{
+		if(err){
+			console.log(err);
+			res.redirect("back");
+		}else{
+			res.redirect("/camps/" + req.params.id );
+		}
+	});
+});
+
+
+//auth session checker middleware function
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
+
+
+
+
+module.exports = router;
---
+++ v7D/seeds.js
@@ -0,0 +1,56 @@
+const mongoose		=	require("mongoose");
+const Campground	=	require("./models/Campground");
+const Comments		=	require("./models/Comments");
+
+//module export
+module.exports = seedDB;
+
+//create data based on the campground model
+var data =[{
+			name: "Heide-Park Holiday Camp",
+			image: "https://media-cdn.tripadvisor.com/media/photo-w/0f/c9/e1/b2/heide-park-holiday-camp.jpg",
+			desc: "#2 of 18 Specialty Lodging in Soltau"
+		},
+	   {
+		   	name: "Tropical Islands",
+			image: "https://media-cdn.tripadvisor.com/media/photo-w/0e/e0/84/4c/tropical-islands-lagune.jpg",
+			desc: "Come in and discover the delights of Tropical Islands. Stroll along the sandy beach by the Tropical Sea, 				join an expedition through the Tropical Rainforest, relax in the Tropical Sauna & Spa Complex, experience 					exciting attractions for kids, have fun with the whole family, taste the wonderful food and enjoy some first-				class entertainment. You will also find that we have various accommodation options for you."
+	   },
+	   {
+		   	name: "Camping am Moslepark",
+			image: "https://media-cdn.tripadvisor.com/media/photo-w/06/65/d4/e9/vista-della-reception.jpg",
+			desc: "Waldseestr. 77, 79117 Freiburg im Breisgau, Baden-Wurttemberg Germany"
+	   }
+	  ];
+
+//create a func seedDB
+function seedDB(){
+	//Delete teh existing data
+	Campground.remove({}, (err)=>{
+		if(err){
+			console.log(err);
+		}else{
+			//create data using the existing data list.
+			// data.forEach(function(seed){
+			// 	Campground.create(seed, (err, campground)=>{
+			// 		if(err){
+			// 			console.log(err);
+			// 		}else{
+			// 			Comments.create({
+			// 				text: "Nice loction, but I wished there was internet.",
+			// 				author: "John Doe"
+			// 			}, (err, comment)=>{
+			// 				if(err){
+			// 					console.log(err);
+			// 				}else{
+			// 					console.log("comment added!");
+			// 					campground.comments.push(comment);
+			// 					campground.save();
+			// 				}
+			// 			});
+			// 		}
+			// 	})
+			// })
+		}
+	})
+}
